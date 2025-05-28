2b60c7ba8eb11d1b194b2df36d99475af9b9f2c3
Luo_Book <153194062+Whitewater8512@users.noreply.github.com>
2025-05-28 20:11:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,8 @@ DATA_DIR = os.path.join(BASE_DIR, 'EuroSAT_MS')
 
 # 权重保存路径
 WEIGHT_DIR = os.path.join(BASE_DIR, 'Weight')
+os.makedirs(WEIGHT_DIR, exist_ok=True)
 
 # 结果保存路径
 RESULT_DIR = os.path.join(BASE_DIR, 'Result')
-
-# 接口路径
-INTERFACE_DIR = os.path.join(BASE_DIR, 'Interface')
+os.makedirs(RESULT_DIR, exist_ok=True)
